70954b9532d7bb6e5cbcaa1e66ec171cd094124c
Linix Red <linus.ordowski@outlook.com>
2025-04-25 20:09:06 +0200

    Initial commit
    Added files

---
+++ README.md
@@ -0,0 +1,149 @@
+# DotDB - Leightweight Local JSON Database
+DotDB is a easy to use and leightweight local database that uses JSON to store data. It also supports different "directories" by usings dots. [See this.](#what-are-dot-directories)  
+You can install it by running:
+
+```sh
+npm install dotdb
+```
+
+# Getting started
+
+To create a database, you need to import dotdb and create a new instance of the class.
+
+```typescript
+import DotDB from "dotdb";
+
+const database = new DotDB("./path-to-your-db.json");
+```
+
+# Methods
+
+There are ten different methods to use.  
+`set, multiset, delete, multidelete, get, has, keys, values, all, clear`  
+  
+All the tutorials below will assume that the db is:
+```json
+{
+    "1": "2",
+    "key": "value",
+    "user_723": {
+        "name": "John Doe",
+        "age": 30,
+        "email": "johndoe@example.com",
+        "address": {
+            "street": "123 Main St",
+            "city": "Anytown",
+            "state": "CA",
+            "zip": "12345"
+        }
+    }
+}
+```
+  
+### Set and Multiset
+
+`set` allows you to set a key to a value in the database
+```typescript
+database.set("k", "v")
+```
+`multiset` allows you to set multiple keys and values at once
+```typescript
+database.multiset({ "k": "v", "k2": "v2" })
+```
+
+### Delete and Multidelete
+`delete` allows you to remove a key from the database
+```typescript
+database.delete("key")
+```
+`multidelete` allows you to delete multiple keys at once
+```typescript
+database.multidelete(["1", "key", "user_723"])
+```
+
+### Get and Has
+`get` allows you to get a value from a key from the database.  
+Make sure to use await before.
+```typescript
+const value = await database.get("key")
+console.log(value) // -> "value"
+```
+`has` allows you to check if a key-value pair exists in the database and returns true if it exists and false if not. Make sure to use await before.
+```typescript
+console.log( await database.has("key") ); // -> true
+console.log( await database.has("abc") ); // -> false
+```
+
+### Keys and Values
+`keys` allows you to get all keys from the database.  
+Make sure to use await before.
+```typescript
+console.log( await database.keys() ); // ->
+// [
+//   '1',
+//   'key',
+//   'user_723.name',
+//   'user_723.age',
+//   'user_723.email',
+//   'user_723.address.street',
+//   'user_723.address.city',
+//   'user_723.address.state',
+//   'user_723.address.zip'
+// ]
+```
+`values` allows you to get all values from the database.  
+Make sure to use await before
+```typescript
+console.log( await database.values() ); // ->
+// [
+//   '2',
+//   'value',
+//   'John Doe',
+//   30,
+//   'johndoe@example.com',
+//   '123 Main St',
+//   'Anytown',
+//   'CA',
+//   '12345'
+// ]
+```
+
+### All and Clear
+`all` returns the entire database as a Record.  
+Make sure to use await before
+```typescript
+console.log( await database.all() ); // ->
+// {
+//   '1': '2',
+//   key: 'value',
+//   user_723: {
+//     name: 'John Doe',
+//     age: 30,
+//     email: 'johndoe@example.com',
+//     address: {
+//       street: '123 Main St',
+//       city: 'Anytown',
+//       state: 'CA',
+//       zip: '12345'
+//     }
+//   }
+// }
+```
+
+`clear` clears the entire database and replaces it with {}.  
+You need to confirm it when writing the function.
+```typescript
+await db.clear(true) // -> `true` is to confirm
+```
+
+## What are "dot directories"?
+Dot directories are kind of like paths. For example insead of doing this:
+```typescript
+database.set("player", { health: 100 })
+```
+You can just simply do this:
+```typescript
+database.set("player.health", 100)
+```
+Those two will have the same result.  
+You can use those "dot direcotries" on all functions.
---
+++ src/dotdb.ts
@@ -0,0 +1,381 @@
+import fs from "fs";
+
+class DotDB {
+    path: string;
+
+    constructor(path: string) {
+        this.path = path;
+
+        this.validate();
+    }
+
+    validate = async () => {
+        
+        try {
+            const data = await fs.readFileSync(this.path, "utf8");
+        } catch ( error ) {
+            await fs.writeFileSync(this.path, JSON.stringify({}));   
+        }
+
+        try {
+            const data = await fs.readFileSync(this.path, "utf8");
+            if ( !data.trim() ) {
+                await fs.writeFileSync(this.path, "{}");
+            }
+        } catch ( error ) {
+            throw new Error(`Error setting file up: ${this.path}`);
+        }
+
+    }
+
+    async set(key: string, value: any): Promise<void> {
+        
+        try {
+
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if ( !data.trim() ) {
+                await fs.writeFileSync(this.path, "{}");
+                data = "{}"
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch ( error ) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            const keys = key.split(".");
+            let current = jsonData;
+
+            for (let i = 0; i < keys.length - 1; i++) {
+                if (!current[keys[i]] || typeof current[keys[i]] !== "object") {
+                    current[keys[i]] = {};
+                }
+                current = current[keys[i]];
+            }
+
+            current[keys[keys.length - 1]] = value;
+
+            try {
+                await fs.writeFileSync(this.path, JSON.stringify(jsonData, null, 4));
+            } catch ( error ) {
+                throw new Error(`Error writing file: ${this.path}`);
+            }
+            
+        } catch ( error ) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+
+    }
+
+    async multiset(pairs: Record<string, any>): Promise<void> {
+        try {
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if (!data.trim()) {
+                await fs.writeFileSync(this.path, "{}");
+                data = "{}";
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch (error) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            for (const key in pairs) {
+                const value = pairs[key];
+                const keys = key.split(".");
+                let current = jsonData;
+
+                for (let i = 0; i < keys.length - 1; i++) {
+                    if (!current[keys[i]] || typeof current[keys[i]] !== "object") {
+                        current[keys[i]] = {};
+                    }
+                    current = current[keys[i]];
+                }
+
+                current[keys[keys.length - 1]] = value;
+            }
+
+            try {
+                await fs.writeFileSync(this.path, JSON.stringify(jsonData, null, 4));
+            } catch (error) {
+                throw new Error(`Error writing file: ${this.path}`);
+            }
+        } catch (error) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+    }
+
+    async get(key: string): Promise<any | undefined> {
+
+        try {
+
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if ( !data.trim() ) {
+                await fs.writeFileSync(this.path, "{}");
+                data = "{}"
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch ( error ) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            const keys = key.split(".");
+            let current = jsonData;
+
+            for (let i = 0; i < keys.length; i++) {
+                if (!current || typeof current !== "object") {
+                    return undefined;
+                }
+                current = current[keys[i]];
+            }
+
+            return current;
+
+        } catch ( error ) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+
+    }
+
+    async delete(key: string): Promise<void> {
+
+        try {
+
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if (!data.trim()) {
+                await fs.writeFileSync(this.path, "{}");
+                data = "{}";
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch (error) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            const keys = key.split(".");
+            let current = jsonData;
+
+            for (let i = 0; i < keys.length - 1; i++) {
+                if (!current || typeof current !== "object") {
+                    return;
+                }
+                current = current[keys[i]];
+            }
+
+            delete current[keys[keys.length - 1]];
+
+            try {
+                await fs.writeFileSync(this.path, JSON.stringify(jsonData, null, 4));
+            } catch (error) {
+                throw new Error(`Error writing file: ${this.path}`);
+            }
+
+        } catch (error) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+
+    }
+
+    async multidelete(keys: string[]): Promise<void> {
+        try {
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if (!data.trim()) {
+                await fs.writeFileSync(this.path, "{}");
+                data = "{}";
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch (error) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            for (const key of keys) {
+                const keyParts = key.split(".");
+                let current = jsonData;
+
+                for (let i = 0; i < keyParts.length - 1; i++) {
+                    if (!current || typeof current !== "object") {
+                        break;
+                    }
+                    current = current[keyParts[i]];
+                }
+
+                if (current && typeof current === "object") {
+                    delete current[keyParts[keyParts.length - 1]];
+                }
+            }
+
+            try {
+                await fs.writeFileSync(this.path, JSON.stringify(jsonData, null, 4));
+            } catch (error) {
+                throw new Error(`Error writing file: ${this.path}`);
+            }
+        } catch (error) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+    }
+
+    async clear(confirm: boolean): Promise<void> {
+
+        if ( !confirm ) {
+            throw new Error("Please confirm that you want to clear the database.");
+        }
+
+        try {
+            await fs.writeFileSync(this.path, "{}");
+        } catch ( error ) {
+            throw new Error(`Error writing file: ${this.path}`);
+        }
+
+    }
+
+    async all(): Promise<Record<any, any> | undefined> {
+
+        try {
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if ( !data.trim() || data == "{}" ) {
+                return undefined;
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch ( error ) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            return jsonData;
+
+        } catch ( error ) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+
+    }
+
+    async has(key: string): Promise<boolean> {
+        try {
+            let data = await fs.readFileSync(this.path, "utf8");
+
+            if (!data.trim()) {
+                await fs.writeFileSync(this.path, "{}");
+                data = "{}";
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch (error) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            const keys = key.split(".");
+            let current = jsonData;
+
+            for (let i = 0; i < keys.length; i++) {
+                if (!current || typeof current !== "object" || !(keys[i] in current)) {
+                    return false;
+                }
+                current = current[keys[i]];
+            }
+
+            return true;
+
+        } catch (error) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+    }
+
+    async keys(): Promise<string[]> {
+        try {
+            const data = await fs.readFileSync(this.path, "utf8");
+
+            if (!data.trim()) {
+                await fs.writeFileSync(this.path, "{}");
+                return [];
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch (error) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            const extractKeys = (obj: any, prefix = ""): string[] => {
+                return Object.keys(obj).flatMap(key => {
+                    const fullKey = prefix ? `${prefix}.${key}` : key;
+                    if (typeof obj[key] === "object" && obj[key] !== null) {
+                        return extractKeys(obj[key], fullKey);
+                    }
+                    return fullKey;
+                });
+            };
+
+            return extractKeys(jsonData);
+
+        } catch (error) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+    }
+
+    async values(): Promise<any[]> {
+        try {
+            const data = await fs.readFileSync(this.path, "utf8");
+
+            if (!data.trim()) {
+                await fs.writeFileSync(this.path, "{}");
+                return [];
+            }
+
+            let jsonData;
+
+            try {
+                jsonData = await JSON.parse(data);
+            } catch (error) {
+                throw new Error(`Error parsing JSON: ${error}`);
+            }
+
+            const extractValues = (obj: any): any[] => {
+                return Object.values(obj).flatMap(value => {
+                    if (typeof value === "object" && value !== null) {
+                        return extractValues(value);
+                    }
+                    return value;
+                });
+            };
+
+            return extractValues(jsonData);
+
+        } catch (error) {
+            throw new Error(`Error reading file: ${this.path}`);
+        }
+    }
+
+}
+
+export default DotDB;
